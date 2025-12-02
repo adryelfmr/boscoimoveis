013621e1d7632d765be81c0e0dd890e51b8d84cd
Adryel <adryel_fmr@hotmail.com>
2025-12-02 15:49:18 -0300

    sitemap

--- public/sitemap-generator.mjs
+++ public/sitemap-generator.mjs
@@ -51,10 +51,7 @@ async function gerarSitemap() {
     const imoveis = response.documents;
     
     // ✅ CORRIGIDO: XML com namespace correto
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-`;
+    let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`;
     
     // Adicionar páginas estáticas
     paginasEstaticas.forEach(pagina => {
